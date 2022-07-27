13a59e7c2647515d06e494486ed8da30ee55f3a9
Vinícius Almeida de Mattos <vinicius.mattos131@gmail.com>
2022-07-27 20:35:50 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Luiz Otávio',
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre 5')
